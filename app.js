cc24d6d214f693f0de44266d479e5c2c070deb33
bernard.phua <bernard.phua@icofgroup.com>
2020-04-12 13:23:34 +0800

    Completed Whack A Mole Game

---
+++ app.js
@@ -0,0 +1,62 @@
+const square = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelector('#time-left');
+let score = document.querySelector('#score');
+
+const totalTime = 5;
+let result = 0;
+let currentTime = totalTime;
+let moveMoleTimer = null;
+let countDownTimer = null;
+
+timeLeft.textContent = totalTime;
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole');
+    });
+
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add('mole');
+    hitPosition = randomPosition.id;
+}
+
+square.forEach(elem=>{
+    elem.addEventListener('mouseup', () =>{
+        if (elem.id === hitPosition){
+            result += 1;
+            score.textContent = result;
+        }
+    })
+})
+
+function moveMole(){
+    moveMoleTimer = setInterval(randomSquare, 1000);
+}
+
+function countDown(){
+    currentTime--;
+    timeLeft.textContent = currentTime.toString();
+
+    if (currentTime === 0){
+        clearInterval(countDownTimer);
+        clearInterval(moveMoleTimer);
+        alert('GAME OVER! Your final score is ' + result);
+    }
+}
+
+function StartGame(){
+    if (currentTime === totalTime){
+        countDownTimer = setInterval(countDown, 1000);
+        moveMole();
+    }
+}
+
+function ResetGame(){
+    result = 0;
+    currentTime = totalTime;
+    timeLeft.textContent = totalTime;
+    score.textContent = result;
+    randomSquare();
+    StartGame();
+}
